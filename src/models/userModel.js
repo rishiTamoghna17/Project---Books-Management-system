9ef56b581bd1709d24d701ee4dc357cf8cc40521
rishiTamoghna17 <tamoghna171099@gmail.com>
2022-09-21 11:43:06 +0530

    updated by tamoghna

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -23,7 +23,8 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
-      trim: true
+      trim: true,
+      lowercase: true
     },
     password: {
       type: String,
