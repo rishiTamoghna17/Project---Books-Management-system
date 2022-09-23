05ee2e7dbe685bae97ea554e3e571de376c74daf
unknown <vaibhavpimpalkar28@gmail.com>
2022-09-23 18:49:21 +0530

    userController

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,51 +1,89 @@
+
 const userModel = require('../models/userModel')
 const jwt = require("jsonwebtoken")
-const {validBody,validUserTitle,validName,validPhone,validMail,validPassword,validAddress} = require('../validator/validator.js')
+const { validBody, validUserTitle, validName, validPhone, validMail, validPassword, validAddress } = require('../validator/validator.js')
 
-const createUser = async function (req,res) {
-    try{
+const createUser = async function (req, res) {
+    try {
         let data = req.body
         // ----- validation start -----------
-        if (!validBody(data)) return res.status(400).send({ status: false, message: "Request body can't be empty" })
-        let {title,name,phone,email,password,address} = data
-        if(!validUserTitle(title)) return res.status(400).send({ status: false, message: "Enter title in valid format" })
-        if(!validName(name)) return res.status(400).send({ status: false, message: "Enter name in valid format" })
-        if(!validPhone(phone)) return res.status(400).send({ status: false, message: "Enter phone in valid format" })
-        let phoneExist = await userModel.findOne({phone:phone})
-        if(phoneExist) return res.status(400).send({ status: false, message: "Phone is already registered" })
-        if(!validMail(email)) return res.status(400).send({ status: false, message: "Enter mail in valid format" })
-        let mailExist = await userModel.findOne({email:email})
-        if(mailExist) return res.status(400).send({ status: false, message: "Email is already registered" })
-        if(!validPassword(password)) return res.status(400).send({ status: false, message: "Enter password in valid format" })
-        if(address){
-            if(!validAddress(address)) return res.status(400).send({ status: false, message: "Enter address in valid format" })
+        if (!validBody(data)) return res.status(400).send({ status: false, message: "Please provide Details " })
+        let { title, name, phone, email, password, address } = data
+
+        if (!title) {
+            return res.status(400).send({ status: false, msg: "Please provide Title.." })
         }
+        if (!validUserTitle(title)) return res.status(400).send({ status: false, message: "please Enter valid Title" })
+
+        if (!validName(name)) return res.status(400).send({ status: false, message: "Please Enter valid Name " })
+        if (!name) {
+            return res.status(400).send({ status: false, msg: "Please provide Name......" })
+        }
+
+        if (!validPhone(phone)) return res.status(400).send({ status: false, message: "Please Enter Valid Mobile Number" })
+        if (!phone) {
+            return res.status(400).send({ status: false, msg: "Please provide Mobile Number" })
+        }
+
+        let phoneExist = await userModel.findOne({ phone: phone })
+        if (phoneExist) return res.status(400).send({ status: false, message: "Phone is already Exist" })
+
+
+        if (!validMail(email)) return res.status(400).send({ status: false, message: "Enter mail in valid format" })
+        if (!email) {
+            return res.status(400).send({ status: false, msg: "Please provide E-mail" })
+        }
+
+        let mailExist = await userModel.findOne({ email: email })
+        if (mailExist) return res.status(400).send({ status: false, message: "Email is already Exist" })
+
+        if (!password) { return res.status(400).send({ msg: "please provode password" }) }
+        if (!validPassword(password)) return res.status(400).send({ status: false, message: "Please Enter Valid Password" })
+
+        if (address) {
+            if (!validAddress(address)) return res.status(400).send({ status: false, message: "Please Enter valid Address" })
+        }
+
         // --------- End ------------
         let savedData = await userModel.create(data)
-        res.status(201).send({status : false , data : savedData})
+        res.status(201).send({ status: false, data: savedData })
     }
-    catch(err){
+    catch (err) {
         res.status(500).send(err.message)
     }
 }
 
-const logIn = async function(req,res){
-    try{
+const logIn = async function (req, res) {
+    try {
         let data = req.body
         // ---------- validations start -----------------
-        if (!validBody(data)) return res.status(400).send({ status: false, message: "Request body can't be empty" })
-        let {email,password} = data
-        if(!validMail(email)) return res.status(400).send({ status: false, message: "Enter mail in valid format" })
-        if(!validPassword(password)) return res.status(400).send({ status: false, message: "Enter password in valid format" })
-        let user = await userModel.findOne({email:email,password:password}).select({"_id":1})
-        if(!user) res.status(400).send({status : false , message : "Invailid mail or password"})
+        if (!validBody(data)) return res.status(400).send({ status: false, message: "Please Provide Id and Password " })
+        let { email, password } = data
+
+
+        if (!email) {
+            return res.status(400).send({ status: false, msg: "Please provide Id" })
+        }
+
+
+        if (!password) {
+            return res.status(400).send({ status: false, msg: "Please provide password " })
+        }
+
+        let Email = await userModel.findOne({ email: email })
+        if (!Email) return res.status(400).send({ status: false, message: "Please Provide correct Login Id" })
+
+        let Password = await userModel.findOne({ password: password })
+        if (!Password) return res.status(400).send({ status: false, message: "Please Provide correct Login Password" })
+
+
         // ------ End -------------
-        let token = jwt.sign({userId:user._id.toString()},"group-38-key-for-login",{ expiresIn: '1h' })
-        res.status(201).send({status : false , data:token})
+        let token = jwt.sign({ userId: user._id.toString() }, "group-38-key-for-login", { expiresIn: '1h' })
+        res.status(201).send({ status: false, data: token })
     }
-    catch(err){
+    catch (err) {
         res.status(500).send(err.message)
     }
 }
 
-module.exports={createUser,logIn}
+module.exports = { createUser, logIn }
--- src/validator/validator.js
+++ src/validator/validator.js
@@ -6,36 +6,30 @@ function validBody(data){
 }
 
 function validId(id){
-    if(!id) return false
     return mongoose.Types.ObjectId.isValid(id)
 }
 
 function validUserTitle(title){
-    if(!title) return false
     if(title.match(/Mr|Miss|Mrs/)) return true
     return false
 }
 
 function validName(name){
-    if(!name) return false
     if(/^[A-Za-z ]{1,20}$/.test(name)) return true
     return false
 }
 
 function validPhone(phone){
-    if(!phone) return false
     if (/^[0]?[6789]\d{9}$/.test(phone)) return true
     return false
 }
 
 function validMail(email){
-    if(!email) return false
     if (/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(email)) return true
     return false
 }
 
 function validPassword(password){
-    if(!password) return false
     if(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/.test(password)) return true
     return false
 }
@@ -47,31 +41,26 @@ function validAddress(address){
 }
 
 function validBookTitle(title){
-    if(!title) return false
     if(/^[A-Za-z]{1,20}/.test(title)) return true
     return false
 }
 
 function validExcerpt(excerpt){
-    if(!excerpt) return false
     if(/^[A-Za-z]{1,100}/.test(excerpt)) return true
     return false
 }
 
 function validISBN(ISBN){
-    if(!ISBN) return false
     if(/^\d{13}$/.test(ISBN)) return true
     return false
 }
 
 function validCategory(category){
-    if(!category) return false
     if(/^[A-Za-z]{1,20}/.test(category)) return true
     return false
 }
 
 function validSubCategory(subcategory){
-    if(!subcategory) return false
     if(/^[A-Za-z]{1,20}/.test(subcategory)) return true
     return false
 }
@@ -82,19 +71,16 @@ function validReviews(reviews){
 }
 
 function validDate(date){
-    if(!date) return false
     if(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/.test(date)) return true
     return false
 }
 
 function validReviewer(reviewedBy){
-    if(!reviewedBy) return false
     if(/^[A-Za-z]{1,20}/.test(reviewedBy)) return true
     return false
 }
 
 function validRating(rating){
-    if(!rating) return false
     if(/^[0-9]{1,5}$/.test(rating)) return true
     return false
 }
